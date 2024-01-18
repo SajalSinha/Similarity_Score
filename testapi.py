012c3d27a67e92abcf4cf2cfabcf112d241dcdc7
Sajal Sinha <79034119+SajalSinha@users.noreply.github.com>
2024-01-18 11:45:10 +0530

    Update testapi.py

--- testapi.py
+++ testapi.py
@@ -34,4 +34,4 @@ def calculate_similarity(data: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
